476c814d1fdefb7ea71fdf1e3679cc9ed9788e3b
Jack McNelis <jjmcnelis@outlook.com>
2019-05-14 15:53:13 -0400

    updates to allow use outside jupyter env

--- ursjupyter.py
+++ ursjupyter.py
@@ -1,51 +1,56 @@
+import sys
 import requests
 import getpass
-from IPython.display import clear_output, HTML
+from IPython.display import display, clear_output, HTML
+
+# if Python2, prompt with raw_input instead of input
+if sys.version_info.major==2:
+    input = raw_input
 
 
 class EarthdataLogin(requests.Session):
     """
-    Prompt user for Earthdata credentials repeatedly until auth success. Test
-    by attempting to download 44kb Daymet granule. Source:
+    Prompt user for Earthdata credentials repeatedly until auth success. Source:
     https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
     """
 
-    AUTH_HOST = "urs.earthdata.nasa.gov"             # urs login url
+    AUTH_HOST = "urs.earthdata.nasa.gov"              # urs login url
     
-    ERROR = "Login failed ({0}). Retry or register." # failure message
+    ERROR = "Login failed ({0}). Retry or register."  # failure message
     
-    TEST = ("https://daac.ornl.gov/daacdata/daymet/" # ORNL DAAC Daymet
-            "Daymet_V3_Annual_Climatology/data/"     # granule to touch to
-            "daymet_v3_prcp_annttl_2017_pr.tif")     # test authentication; 
+    TEST = ("https://daac.ornl.gov/daacdata/")        # test authentication; 
     
-    REGISTER = HTML(                                 # registration prompt
-        "<p style='font-weight:bold'><a href=https://urs.earthdata.nasa.gov"
-        "/users/new target='_blank'>Click here to register an Earthdata account.</a></p>")
+    REGISTER = HTML(                                  # registration prompt
+        "<p style='font-weight:bold'><a href=https://urs.earth"
+        "data.nasa.gov/users/new target='_blank'>Click here to"
+        " register a NASA Earthdata account.</a></p>")
                               
     
     def __init__(self):
         fails = 0
         
         while True:
-            display(self.REGISTER) # register prompt
+            display(self.REGISTER)                     # register prompt
+            username = input("Username: ")             # username prompt 
+            password = getpass.getpass("Password: ")   # secure pw prompt
             
-            username = input("Username: ")            # prompt for username
-            password = getpass.getpass("Password: ")  # secure prompt for pw
-            
-            super().__init__()                        # init requests session
-            self.auth = (username, password)          # add user,pw
+            if sys.version_info.major==2:              # init requests session
+                super(EarthdataLogin, self).__init__() # for Python 2
+            else:
+                super().__init__()                     # for Python 3
+            self.auth = (username, password)           # add username,password
             
             try:                                     
-                response = self.get(self.TEST)        # try to grab TEST
-                response.raise_for_status()           # raise for status>400
-                clear_output()                        # clear output
+                response = self.get(self.TEST)         # try to grab TEST
+                response.raise_for_status()            # raise for status>400
+                clear_output()                         # clear output
                 display("Login successful. Download with: session.get(url)")
                 break
 
             except requests.exceptions.HTTPError as e:
-                clear_output()                        # clear cell output
-                fails += 1                            # +1 fail counter
-                display(self.ERROR.format(str(fails)))# print failure msg
+                clear_output()                         # clear cell output
+                fails += 1                             # +1 fail counter
+                display(self.ERROR.format(str(fails))) # print failure msg
 
     
     def rebuild_auth(self, prepared_request, response):
@@ -66,8 +71,8 @@ class EarthdataLogin(requests.Session):
                     original_parsed.hostname != self.AUTH_HOST:
                 del headers['Authorization']
                    
-        #self.auth = None                    # purge username/password inputs
-        return                              # return requests.Session object
+        self.auth = None   # drop username/password attributes
+        return             # return requests.Session
 
 
 ### RUN ON IMPORT
